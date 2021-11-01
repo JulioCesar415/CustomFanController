150d9638bc899eaa173ca3f6dd1017cfc29a6431
JulioCesar415 <35044318+JulioCesar415@users.noreply.github.com>
2021-11-01 16:38:45 -0700

    build fan controller

--- app/src/main/java/com/example/customfancontroller/DialView.kt
+++ app/src/main/java/com/example/customfancontroller/DialView.kt
@@ -1,2 +1,45 @@
 package com.example.customfancontroller
 
+import android.content.Context
+import android.graphics.Paint
+import android.graphics.PointF
+import android.graphics.Typeface
+import android.util.AttributeSet
+import android.view.View
+
+//top-level enum to represent the available speeds.
+//values are of type Int because the values are string resource rather than actual strings
+private enum class FanSpeed(val label: Int) {
+    OFF(R.string.fan_off),
+    LOW(R.string.fan_low),
+    MEDIUM(R.string.fan_medium),
+    HIGH(R.string.fan_high);
+}
+
+//constants used as part of drawing the dial indicator and labels
+private const val RADIUS_OFFSET_LABEL = 30
+private const val RADIUS_OFFSET_INDICATOR = -35
+
+class DialView @JvmOverloads constructor(
+    context: Context,
+    attrs: AttributeSet? = null,
+    defStyleAttr: Int = 0
+) : View(context, attrs, defStyleAttr){
+
+//    define several variables you need in order to draw the custom view
+//    radius of the circle
+    private var radius = 0.0f
+//    active selection
+    private var fanSpeed = FanSpeed.OFF
+//    position variable which will be used to draw label and indicator circle position
+    private val pointPosition: PointF = PointF(0.0f, 0.0f)
+
+//    initialize paint object with handful of basic styles.
+//    these styles are initialized here to help speed up the drawing step
+    private val paint = Paint(Paint.ANTI_ALIAS_FLAG).apply {
+        style = Paint.Style.FILL
+        textAlign = Paint.Align.CENTER
+        textSize = 55.0f
+        typeface = Typeface.create("", Typeface.BOLD)
+    }
+}
